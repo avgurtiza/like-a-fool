545af83aca708d3a8a41dd4f2161897e3ed567a2
Slide Gurtiza <slide@teleserv.ph>
2021-03-26 20:51:50 +0800

    It moves!

--- Assets/Scripts/BeatController.cs
+++ Assets/Scripts/BeatController.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class BeatController : MonoBehaviour
 {
+    protected GameObject BakaMitaiVid;
+    
+    private Dictionary<double, GameObject> beatMap;
+
+    private double lastTime;
+
+    public string TextMe;
+    
+    public GameObject KeyPrefabI;
+    public GameObject KeyPrefabJ;
+    public GameObject KeyPrefabK;
+    public GameObject KeyPrefabL;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("INSIDE BEATMAP CONTROLLER");   
+
+        beatMap =  new Dictionary<double, GameObject>();
+        beatMap.Add(2.6d, KeyPrefabI);
+        beatMap.Add(7.6d, KeyPrefabI);
+        beatMap.Add(9d, KeyPrefabJ);
+        beatMap.Add(11d, KeyPrefabK);
         
+        BakaMitaiVid = GameObject.FindWithTag("SongVid");
     }
 
     // Update is called once per frame
     void Update()
     {
+        var time = BakaMitaiVid.GetComponent<VideoPlayer>().time;
         
+        if(beatMap.ContainsKey(time) && time > lastTime) {
+            lastTime =  time;
+            GameObject thisKey = Instantiate(beatMap[time], new Vector3(357, 0, 0), Quaternion.identity);
+
+            thisKey.transform.SetParent (GameObject.Find("BeatCanvas").transform, false);
+        }
     }
+    
 }
---
+++ Assets/Scripts/KeyMotionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyMotionController : MonoBehaviour
+{
+    private float keyXPosition;
+    public float keySpeed = 50f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("KEY HAPPENED!");
+        keyXPosition = gameObject.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keySpeed = Time.deltaTime;
+        transform.Translate(Vector2.left * keySpeed);
+    }
+}
